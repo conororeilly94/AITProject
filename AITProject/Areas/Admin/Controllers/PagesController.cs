43fc0a6fbda6bd629ae80ed093ce8de0bc99a5b5
Conor O Reilly <x17101701@student.ncirl.ie>
2019-11-03 17:24:35 +0000

    Add Page function

---
+++ AITProject/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,85 @@
+﻿using AITProject.Models.Data;
+using AITProject.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AITProject.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Declare list of Page View Models
+            List<PageVM> pagesList;
+
+            //Init the list
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Return view with list
+            return View(pagesList);
+        }
+
+        // GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        // GET: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //Check model state
+            if (! ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                //Declare slug
+                string slug;
+
+                //Init pageDTO
+                PageDTO dto = new PageDTO();
+
+                //DTO title
+                dto.Title = model.Title;
+
+                //Check for and set slug if needed
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace("", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace("", "-").ToLower();
+                }
+
+                //Make sure title and slug are unique
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+
+                }
+
+                //DTO the rest
+
+                //Save DTO
+
+            }
+
+            //Set TempData message
+
+            //Redirect
+            return View();
+        }
+    }
+}
---
+++ AITProject/Models/Data/Db.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AITProject.Models.Data
+{
+    public class Db : DbContext
+    {
+        public DbSet<PageDTO> Pages { get; set; }
+    }
+}
