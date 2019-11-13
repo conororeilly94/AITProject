e7d7ebf95caff0257c955313f8cca4bedf929358
Conor O Reilly <x17101701@student.ncirl.ie>
2019-11-13 20:07:32 +0000

    Add and delete categories

--- AITProject/Areas/Admin/Controllers/ShopController.cs
+++ AITProject/Areas/Admin/Controllers/ShopController.cs
@@ -29,5 +29,63 @@ namespace AITProject.Areas.Admin.Controllers
             //Return view with list
             return View(categoryVMList);
         }
+
+        // POST: Admin/Shop/AddNewCategories
+        [HttpPost]
+        public string AddNewCategories(string catName)
+        {
+            //Declare id
+            string id;
+
+            using (Db db = new Db())
+            {
+                //Check category name is unique
+                if (db.Categories.Any(x => x.Name == catName))
+                    return "titletaken";
+
+                //Init DTO
+                CategoryDTO dto = new CategoryDTO();
+
+                //Add to DTO
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                //Save to DTO
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                //Get the id
+                id = dto.Id.ToString();
+            }
+
+            //Return id
+            return id;
+        }
+
+        //GET: Admin/Pages/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                //Set inital count
+                int count = 1;
+
+                //Declare pageDTO
+                CategoryDTO dto;
+
+                //Set sorting for each category
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+        }
     }
 }
